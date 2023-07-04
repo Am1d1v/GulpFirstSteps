9f4a0b9630a767e60d6568b9cabd40c848f04a0f
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-04 21:11:42 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,25 @@ const gulp = require('gulp');
 const sass = require('sass');
 const del = require('del');
 
+const paths = {
+    styles: {
+      src: 'src/styles/style.sass',
+      dest: 'dist/css'
+    },
+    scripts: {
+      src: 'src/scripts/*.js',
+      dest: 'dist/js'
+    }
+}
+
 function clean(){
   return del(['dist'])
 }
-
 exports.clean = clean
+
+function styles(){
+  return gulp.src(paths.styles.src)
+  .pipe(sass())
+  .pipe(gulp.dest(paths.styles.dest))
+}
+exports.styles = styles
